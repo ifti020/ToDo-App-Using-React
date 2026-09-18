7c6ef663e9a083ec0840a5121a0879f7a3b24474
ïfti020 <iftihaque1@gmail.com>
2026-09-18 20:39:22 +0600

    Add state management and handle submission in component @NewTodoForm

--- react-todo-demo/react-todos/src/components/NewTodoForm.jsx
+++ react-todo-demo/react-todos/src/components/NewTodoForm.jsx
@@ -1,15 +1,45 @@
+import {useState} from 'react'
 export function NewTodoForm()
 {
+    const [assigned, setAssigned] = useState('')
+    const[description, setDescription] = useState('')
+
+    const handleSubmit =(e) =>{
+        e.preventDefault();
+
+        if(assigned.trim() && description.trim()){
+            console.log(assigned)
+            console.log(description)
+
+            setAssigned('')
+            setDescription('')
+        }
+
+    }
+
+
     return (
         <div>
-            <form>
+            <form onSubmit={handleSubmit}>
             <div>
                 <label>Assigned Task</label>
-                <input type = 'text' required />
+                <input
+                    type = "text"
+                    required
+                    value={assigned}
+                    onChange={(e) =>
+                    { setAssigned(e.target.value)}}/>
             </div>
                 <div>
                     <label>Task Description</label>
-                    <textarea rows={4} required></textarea>
+                    <textarea
+                        rows={4}
+                        required
+                        value={description}
+                    onChange={(e)=>
+                    { setDescription(e.target.value) }}>
+
+                    </textarea>
                 </div>
                 <button> Add Todo</button>
             </form>
